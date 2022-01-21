feadbf1e8529528b3ddcc50f8b24185f74424ff8
Elsombrerobot <iammarcferrer@gmail.com>
2022-01-21 08:28:41 +0100

    add stuff for basic setuptool

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="image_frame",
+    version="0.1",
+    description="rame your images.",
+    url="https://github.com/Elsombrerobot/image_frame",
+    author="Marc Ferrer",
+    author_email="iammarcferrer@gmail.com",
+    license="MIT",
+    packages=["image_frame"],
+    zip_safe=False,
+)
