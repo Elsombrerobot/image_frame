d9c62b6895f9e5a6ed081476b0d3b5ffbe74e6be
Elsombrerobot <iammarcferrer@gmail.com>
2022-01-23 14:13:31 +0100

    fix : changing setuptool for twine

--- image_frame/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-"""Configure global variable for the app."""
-
-RESSOURCES = Path(__file__).parents[1] / "resources"
-FONT_PATH = RESSOURCES / "CutiveMonoRegular.ttf"
-VALID_EXT = [".jpg", ".png", ".jpeg"]
