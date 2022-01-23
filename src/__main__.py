e798b1447d568f30a62802b0c20fbd31ec610064
Elsombrerobot <iammarcferrer@gmail.com>
2022-01-23 14:17:18 +0100

    fix : re add files.

---
+++ src/__main__.py
@@ -0,0 +1,134 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Frame images . Work only with jpeg and png.",
+    usage="image_frame [options], use @file.txt for text file arguments.",
+    epilog="Mainly used for instagram.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    fromfile_prefix_chars="@",
+)
+
+parser.add_argument(
+    "-i",
+    "--input",
+    dest="input",
+    type=str,
+    help="The input to process. If is dir, will process all the jpegs.",
+)
+
+parser.add_argument(
+    "-o",
+    "--output",
+    dest="output",
+    type=str,
+    help=(
+        "Exemple : ./{filename}', output_file, .././some/dir/{filename}"
+        " (relative to input). Will create trees and override files. If not"
+        " specified, output in the input file dir as {filename}_framed. Only"
+        " {filename} is available as key."
+    ),
+)
+
+parser.add_argument(
+    "-s",
+    "--size",
+    dest="size",
+    type=int,
+    default=0,
+    help=(
+        "Specify the size of the final image, if not specified, original size"
+        " is kept and the program just add the frame, so it will be bigger."
+    ),
+)
+
+parser.add_argument(
+    "-fs",
+    "--frame-size",
+    dest="frame_size",
+    type=int,
+    default="5",
+    help=(
+        "Specify the size of the frame, as precentage of the greater size of"
+        " the image."
+    ),
+)
+
+parser.add_argument(
+    "-fsh",
+    "--frame-shape",
+    dest="frame_shape",
+    default="same",
+    type=str,
+    help="Specify the shape of the frame. Values : same, square.",
+)
+
+parser.add_argument(
+    "-fc",
+    "--frame-color",
+    dest="color",
+    type=str,
+    default="white",
+    help=(
+        "Specify the color of the frame. Values : median (see -ac,"
+        " --available_colors for all colors) (to implement : acrylic, median)."
+    ),
+)
+
+parser.add_argument(
+    "-te",
+    "--tag-exifs",
+    dest="exif_loc",
+    type=str,
+    help=(
+        "Tag the frame with exif tag at the given location if one location arg"
+        " is specified. Values : top, bottom"
+    ),
+)
+
+parser.add_argument(
+    "-d",
+    "--description",
+    dest="description",
+    nargs=2,
+    type=str,
+    help=(
+        "Tag the frame with given string at the given location if one location"
+        " arg is specified. Locations : top, bottom. Take 2 args, exemple : -d"
+        " 'Cool description' bottom"
+    ),
+)
+
+parser.add_argument(
+    "-ftw",
+    "--font-size-weight",
+    dest="font_size_weight",
+    default=1,
+    type=float,
+    help="Specify the multiplier for the font size.",
+)
+
+parser.add_argument(
+    "-fco",
+    "--font-color",
+    dest="font_color",
+    default="black",
+    type=str,
+    help="Specify the color of the font.",
+)
+
+parser.add_argument(
+    "-ac",
+    "--available-colors",
+    dest="available_colors",
+    action="store_true",
+    help=(
+        "Print all the available color options for --frame_color and"
+        " --font-color, skip any other arg."
+    ),
+)
+
+if __name__ == "__main__":
+    from core import main
+
+    args = parser.parse_args()
+    main(args)
---
+++ src/process_image.py
@@ -0,0 +1,197 @@
+from pathlib import Path
+from pprint import pprint
+
+from PIL import ExifTags, Image, ImageDraw, ImageFont, ImageOps
+
+from config import FONT_PATH
+from utils import average_color, image_orientation
+
+
+def exif_dict(filepath):
+    """Dict with simple exif data.
+
+    Parameters
+    ----------
+    filepath : str, Path
+        The filepath of the image to analyse.
+
+    Returns
+    -------
+    dict
+        Data with simple exif data.
+    """
+    img = Image.open(filepath, "r")
+
+    exif = {
+        ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in ExifTags.TAGS
+    }
+
+    # Exposure time
+    if exif["ExposureTime"] < 1:
+        exposure = f"1/{int(1/exif['ExposureTime'])}"
+    else:
+        exposure = f"1/{int(exif['ExposureTime']/1)}"
+
+    # Model
+    model = exif["Model"].lower()
+
+    # Iso
+    iso = f"iso {exif['ISOSpeedRatings']}"
+
+    # Focal length
+    focal_length = f"{exif['FocalLengthIn35mmFilm']} mm"
+
+    # fstop
+    if float(exif["FNumber"]).is_integer():
+        fstop = f"f/{int(exif['FNumber'])}"
+    else:
+        fstop = f"f/{exif['FNumber']}"
+
+    return {
+        "exposure": exposure,
+        "model": model,
+        "iso": iso,
+        "f_number": fstop,
+        "focal_length": focal_length,
+    }
+
+
+def resize_img(img: Image, img_size):
+    """
+    Resize an image to the given size.
+
+    Return
+    ------
+    Image
+    """
+    img = ImageOps.contain(img, (img_size, img_size), method=Image.LANCZOS)
+    return img
+
+
+def add_frame(img: Image, color, size, shape, acrylic=False):
+    """Add a frame with the given pixel size, color, and shape.
+
+    Parameters
+    ----------
+    img : Image
+        Input image.
+    color : str
+        The color of the frame.
+    size : int
+        The size of the frame in px.
+    shape : str
+        The shape of the frame. Values : same, square.
+    catrylic : Bool
+        Use a blurred version of the image for the frame.
+
+    Return
+    ------
+    Image
+    """
+    frame_color = average_color(img) if color == "median" else color
+
+    if shape == "same":
+        border = size
+    elif shape == "square":
+        square_size = round((max(img.size) + size * 2 - min(img.size)) / 2)
+
+        if image_orientation(img) == "portrait":
+            border = (square_size, size, square_size, size)
+        else:
+            border = (size, square_size, size, square_size)
+
+    img = ImageOps.expand(img, border=border, fill=frame_color)
+
+    return img
+
+
+def tag_text(
+    img: Image,
+    text,
+    location,
+    font_size,
+    font_color,
+):
+
+    font = ImageFont.truetype(str(FONT_PATH), round(font_size))
+    fw, fh = font.getsize(text)
+
+    if location == "top":
+        offset = (
+            round((img.size[0] / 2) - fw / 2),
+            round(fh / 3),
+        )
+    elif location == "bottom":
+        offset = (
+            round((img.size[0] / 2) - fw / 2),
+            round(img.size[1] - fh - fh / 2),
+        )
+
+    draw = ImageDraw.Draw(img)
+    text = text.lower().strip("'")
+    draw.text(offset, text.lower(), font=font, fill=font_color)
+    return img
+
+
+def tag_exifs(
+    img: Image, img_path, location, font_size, font_color, border_size
+):
+    """Add an exifs inscription in the frame at given location."""
+    exifs = exif_dict(img_path)
+    exif_stamp = (
+        exifs["model"]
+        + " "
+        + exifs["focal_length"]
+        + " "
+        + exifs["f_number"]
+        + " "
+        + exifs["exposure"]
+        + " "
+        + exifs["iso"]
+    )
+    return tag_text(img, exif_stamp, location, font_size, font_color)
+
+
+def process_image(source: Path, dest: Path, args):
+    """Process the file given args."""
+    img = Image.open(source)
+
+    # Add frame to the image
+    percentaged_border_size = round(max(img.size) / 100) * args.frame_size
+    img = add_frame(img, args.color, percentaged_border_size, args.frame_shape)
+
+    # Resize the image
+    if args.size:
+        img = resize_img(img, args.size)
+
+    # Generate font tsize if not specified
+    font_size = round((max(img.size) / 35) * args.font_size_weight)
+
+    # Add exifs inscription to the image
+    if args.exif_loc:
+        try:
+            img = tag_exifs(
+                img,
+                source,
+                args.exif_loc,
+                font_size,
+                args.font_color,
+                percentaged_border_size,
+            )
+        except AttributeError:
+            print("Error while tagging exifs, skipping exif tag.")
+
+    # Add custom text inscription to the image
+    if args.description:
+
+        img = tag_text(
+            img,
+            args.description[0],
+            args.description[1],
+            font_size,
+            args.font_color,
+        )
+
+    img.save(dest)
